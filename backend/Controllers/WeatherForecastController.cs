e195af0b48f914c96703b77dcb6e74fdca3f9b67
Gagneet Singh <gagneet@gmail.com>
2020-08-02 20:43:23 +1000

    Updated code for frontend to connect to backend.

--- backend/Controllers/WeatherForecastController.cs
+++ backend/Controllers/WeatherForecastController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace backend.Controllers
 {
@@ -24,16 +26,29 @@ namespace backend.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public async Task<string> Get([FromServices]IDistributedCache cache)
+        {
+        var weather = await cache.GetStringAsync("weather");
+
+        if (weather == null)
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+
+            weather = JsonSerializer.Serialize(forecasts);
+
+            await cache.SetStringAsync("weather", weather, new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(5)
+            });
+        }
+        return weather;
         }
     }
 }
